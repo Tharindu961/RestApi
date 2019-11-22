ac1803cd2b15b3e4fb3c4aa03e9c1f4ae5208042
Tharindu Bandara <37532030+Tharindu961@users.noreply.github.com>
2019-11-22 22:22:57 +0530

    rest api
    this is my first api

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+];
 
 app.get('/', (req, res) => {
     res.send('Hello Wold Sri lanka');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body); // result.error
+
+    if (result.error) {
+        //400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.put('/api/courses/:id', (req,res) => {
+    //Look uo the course
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course with the given ID wasnot found.');
+    
+    //If not existing, return 404
+
+    //Validagte
+
+    //If invalid, return 400 - Bad request
+    
+    const { error } = validateCourse(req.body); // result.error
+
+    if (result.error) {
+        //400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    
+
+    //Update course
+    course.name =req.body.name;
+    //Return the updated course
+    res.send(course);
+
+});
+
+function validateCourse(course) {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+         return Joi.validate(req.body, schema);
+
+}
+
+
+
+
+app.get('/api/courses/:id', (req, res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course with the given ID wasnot found.');
+   res.send(course);    
 });
 
 //Port
